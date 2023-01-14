d86214a1ba5ebdb75ec36e015db86ffca3b37ed0
Haripriya Chittari <hpvarma09@gmail.com>
2023-01-14 12:24:42 +0530

    laying foundation

--- Chapter-Laying_foundation/App.js
+++ Chapter-Laying_foundation/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "heading", key: "h1" },
-  "Laying the foundation"
-);
+
+// console.log(heading);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="logo">LOGO</h1>
+      <div className="right">
+        <input className="input" type="text" />
+        <h1>Haripriya</h1>
+        <img
+          className="image"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="pi"
+        />
+      </div>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Header />);
